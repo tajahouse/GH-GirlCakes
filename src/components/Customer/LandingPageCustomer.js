b9d134496b920bd9bdfad04b9857455adc4596a8
Taja House <ahlliahtaja@yahoo.com>
2022-02-06 11:39:32 +0100

    Started README.md and checked functionality of app, all good

---
+++ src/components/Customer/LandingPageCustomer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPageCustomer = () => {
+  return (
+    <>
+      <h1>Customer Landing Page</h1>
+    </>
+  );
+};
+
+export default LandingPageCustomer;
