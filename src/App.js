ddded3c47b7a2ed2e9f4fc2d426befcc9fe3d365
Taja House <ahlliahtaja@yahoo.com>
2022-02-09 14:54:01 +0100

    Completed Carousel with placeholder images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import LandingPageCustomer from './components/Customer/LandingPageCustomer';
-
-
+import { CarouselImgs } from './components/Styles/Cards'
+  
 function App() {
-  return (
+ 
+    return (
     <div className="App">
       <LandingPageCustomer/>
+      <CarouselImgs/>
+       
     </div>
   );
 }
-
+ 
 export default App;
