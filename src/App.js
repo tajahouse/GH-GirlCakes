1ef55b5fe41819b51e98f497312bfce41add55b0
tajahouse <ahlliahtaja@yahoo.com>
2022-07-03 13:31:25 +0200

    Integrating nextjs in project

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import NavBar from './components/NavBar';
 
 
 function App() {
- 
+ //For later use, create state for roles of users and allow the landing page to be based on the current user. 
     return (
     <div className="App">
       <NavBar/>
+      {/* [customer, admin, superuser].includes(state of current user) : Method will determine the user role and change the landing page */}
       <LandingPageCustomer/>
       <CarouselImgs/>
        
